c125dfc5826b9ad2fd456df763ff7a33338482b8
naelrouz <nael_rouz@mail.ru>
2016-07-29 13:50:45 +0300

    test

--- test.php
+++ test.php
@@ -1,33 +1,91 @@
-<?php
-
-function mergeSort(array $arr)
-{
-    // BEGIN (write your solution here)
-    if()
-    $left = array_slice(0,)
-    $right
-    return merge($left, $right);
-    // END
-}
-
-function merge($left, $right)
-{
-    $result = array();
-
-    while (count($left) > 0 && count($right) > 0) {
-        if ($left[0] <= $right[0]) {
-            array_push($result, array_shift($left));
-        } else {
-            array_push($result, array_shift($right));
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: black;
+            margin: 0;
+            padding: 0px;
         }
-    }
 
-    array_splice($result, count($result), 0, $left);
-    array_splice($result, count($result), 0, $right);
+        .wrap {
+            padding-left: 100px;
+            text-align: center;
+        }
 
-    return $result;
-}
+        .wrap ul {
+            position: relative;
+            padding: 0;
+            margin: 0;
+            /*border: 1px dotted #777;*/
+        }
 
-print_r(mergeSort([8, 7, 9, 3, 7, 5, 8, 7, 3, 5, 7, 8, 9, 3, 4]));
+        li {
 
-//mergeSort([8, 7, 9, 3, 7, 5, 8, 7, 3, 5, 7, 8, 9, 3, 4]);
+            position: relative;
+
+
+            margin: 0;
+            padding: 0;
+        }
+        li a {
+            /*display: inline-block;*/
+            font-size: 18px;
+            font-weight: bold;
+            position: absolute;
+            left: 0px;
+            top: 0px;
+            color: #fff;
+            text-decoration: none;
+            transition: all .5s ease;
+            letter-spacing: 1px;
+        }
+        li a:hover {
+            letter-spacing: 3px;
+        }
+        a:after {
+            /*display: inline-block;*/
+            position: absolute;
+            top: 60%;
+            bottom: 0;
+            left: -10%;
+            height: 1px;
+            content: " ";
+            text-shadow: none;
+            background-color: #fff;
+            width: 0px;
+            transition: all .5s ease;
+        }
+
+        li:hover a:after {
+            width: 120%;
+        }
+
+
+    </style>
+
+</head>
+<body>
+<div class="wrap">
+    <ul>
+        <li><a href="#one" id="test">Concept</a></li>
+    </ul>
+</div>
+
+
+<script>
+    //    var test = document.getElementById('test');
+    //    test.addEventListener("mouseover", hendler1, false);
+    //    test.addEventListener("mouseout", hendler2, false);
+    //
+    //    function hendler1() {
+    //        test.style.color = 'red';
+    //    }
+    //    function hendler2() {
+    //        test.style.color = 'white';
+    //    }
+</script>
+</body>
+</html>
